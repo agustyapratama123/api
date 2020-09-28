61310f6053c3b5a53c73cafdd128cb998a61c86c
agustyapratama123 <agustyapratama123@gmail.com>
2020-09-28 07:17:09 +0700

    modified product details

--- routes/api.php
+++ routes/api.php
@@ -7,7 +7,7 @@ Route::middleware('auth:api')->get('/user', function (Request $request) {
     return $request->user();
 });
 
-Route::apiResource('/Products', App\Http\Controllers\ProductController::class);
+Route::apiResource('/products', App\Http\Controllers\ProductController::class);
 Route::group(['prefix'=>'products'],function(){
     Route::apiResource('/{product}/reviews',App\Http\Controllers\ReviewController::class);
 });
